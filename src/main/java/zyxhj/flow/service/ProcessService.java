de6bd5db65ac3d8d9eb2faec9d1f0985e0316211
JXians <zheng_xing_jiang@163.com>
2019-11-25 09:38:05 +0800

    无用代码删除

--- src/main/java/zyxhj/flow/service/ProcessService.java
+++ src/main/java/zyxhj/flow/service/ProcessService.java
@@ -1,8 +1,6 @@
 package zyxhj.flow.service;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,7 +9,6 @@ import org.slf4j.LoggerFactory;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
